8dee1c20b4817fef79e5b0607f7b29cb313684e5
Ben Ashford <benashford@users.noreply.github.com>
2018-02-23 13:01:47 +0000

    Allow return values

--- examples/counter.rs
+++ examples/counter.rs
@@ -10,7 +10,7 @@ use futures::sync::mpsc;
 
 use futures_cpupool::CpuPool;
 
-use lwactors::{actor, Action};
+use lwactors::{actor, Action, ActorError, ActorSender};
 
 fn main() {
     let cpu_pool = CpuPool::new_num_cpus();
@@ -19,28 +19,30 @@ fn main() {
 
     let a = thread::spawn(move || {
         let futs: Vec<_> = (0..100).map(|i| counter1.add(i)).collect();
-        future::join_all(futs).wait().expect("Futures failed 1");
+        let results = future::join_all(futs).wait().expect("Futures failed 1");
+        println!("ADD RESULTS: {:?}", results);
     });
 
     let counter2 = counter.clone();
 
     let b = thread::spawn(move || {
         let futs: Vec<_> = (0..100).map(|i| counter2.subtract(i)).collect();
-        future::join_all(futs).wait().expect("Futures failed 2");
+        let results = future::join_all(futs).wait().expect("Futures failed 2");
+        println!("SUB RESULTS: {:?}", results);
     });
 
     a.join().expect("Thread one failed");
     b.join().expect("Thread two failed");
-
-    thread::sleep_ms(1000);
 
     println!("Finished: {:?}", cpu_pool);
 }
 
 #[derive(Clone)]
 struct Counter {
-    queue: mpsc::Sender<CounterAction>,
+    queue: ActorSender<CounterAction, i64>,
 }
+
+type CounterFuture = Box<Future<Item = i64, Error = CounterError>>;
 
 impl Counter {
     fn new(cpu_pool: &CpuPool) -> Counter {
@@ -49,12 +51,12 @@ impl Counter {
         }
     }
 
-    fn add(&self, n: i64) -> Send<mpsc::Sender<CounterAction>> {
-        self.clone().queue.send(CounterAction::Add(n))
+    fn add(&self, n: i64) -> CounterFuture {
+        Box::new(self.queue.invoke(CounterAction::Add(n)))
     }
 
-    fn subtract(&self, n: i64) -> Send<mpsc::Sender<CounterAction>> {
-        self.clone().queue.send(CounterAction::Subtract(n))
+    fn subtract(&self, n: i64) -> CounterFuture {
+        Box::new(self.queue.invoke(CounterAction::Subtract(n)))
     }
 }
 
@@ -64,12 +66,22 @@ enum CounterAction {
     Subtract(i64),
 }
 
-impl Action<i64> for CounterAction {
-    fn act(self, state: &mut i64) {
+impl Action<i64, i64> for CounterAction {
+    fn act(self, state: &mut i64) -> i64 {
         println!("Acting {:?} on {}", self, state);
         match self {
             CounterAction::Add(i) => *state += i,
             CounterAction::Subtract(i) => *state -= i,
         }
+        return *state;
+    }
+}
+
+#[derive(Debug)]
+struct CounterError;
+
+impl From<ActorError> for CounterError {
+    fn from(_: ActorError) -> Self {
+        CounterError
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,21 @@
 extern crate futures;
 extern crate futures_cpupool;
 
-use futures::{Async, Future, Poll, Stream};
+use std::error::Error;
+use std::fmt;
+
+use futures::{Async, Future, Poll, Sink, Stream};
 use futures::future::Executor;
-use futures::sync::mpsc;
+use futures::sync::{mpsc, oneshot};
 use futures_cpupool::CpuPool;
 
 const DEFAULT_SIZE: usize = 8;
 
-pub fn actor<A, S>(cpu_pool: &CpuPool, initial_state: S) -> mpsc::Sender<A>
+pub fn actor<A, S, R>(cpu_pool: &CpuPool, initial_state: S) -> ActorSender<A, R>
 where
-    A: Action<S> + Send + 'static,
+    A: Action<S, R> + Send + 'static,
     S: Send + 'static,
+    R: Send + 'static,
 {
     let (tx, rx) = mpsc::channel(DEFAULT_SIZE);
     let actor = Actor {
@@ -21,17 +25,48 @@ where
     cpu_pool
         .execute(actor)
         .expect("Cannot schedule actor on executor");
-    tx
+    ActorSender(tx)
 }
 
-pub struct Actor<A, S> {
-    receiver: mpsc::Receiver<A>,
+type ActorSenderInner<A, R> = mpsc::Sender<(A, oneshot::Sender<R>)>;
+
+#[derive(Debug)]
+pub struct ActorSender<A, R>(ActorSenderInner<A, R>);
+
+impl<A, R> Clone for ActorSender<A, R> {
+    fn clone(&self) -> Self {
+        ActorSender(self.0.clone())
+    }
+}
+
+impl<A, R> ActorSender<A, R>
+where
+    A: 'static,
+    R: 'static,
+{
+    pub fn invoke<E>(&self, action: A) -> ActFuture<R, E>
+    where
+        E: From<ActorError>,
+    {
+        let (tx, rx) = oneshot::channel();
+        let send_f = self.0.clone().send((action, tx));
+        let act_f = send_f.map_err(|e| e.into()).join(rx.map_err(|e| e.into()));
+        Box::new(
+            act_f
+                .map(|(_, result)| result)
+                .map_err(|e: ActorError| e.into()),
+        )
+    }
+}
+
+pub struct Actor<A, S, R> {
+    receiver: mpsc::Receiver<(A, oneshot::Sender<R>)>,
     state: S,
 }
 
-impl<A, S> Future for Actor<A, S>
+impl<A, S, R> Future for Actor<A, S, R>
 where
-    A: Action<S>,
+    A: Action<S, R>,
 {
     type Item = ();
     type Error = ();
@@ -39,7 +74,10 @@ where
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         loop {
             match self.receiver.poll() {
-                Ok(Async::Ready(Some(a))) => a.act(&mut self.state),
+                Ok(Async::Ready(Some((a, tx)))) => {
+                    // Not checking the result, as nothing may be waiting
+                    let _ = tx.send(a.act(&mut self.state));
+                }
                 Ok(Async::Ready(None)) => return Ok(Async::Ready(())),
                 Ok(Async::NotReady) => return Ok(Async::NotReady),
                 Err(()) => return Err(()),
@@ -48,6 +86,43 @@ where
     }
 }
 
-pub trait Action<S> {
-    fn act(self, s: &mut S);
+pub trait Action<S, R> {
+    fn act(self, s: &mut S) -> R;
+}
+
+pub type ActFuture<R, E> = Box<Future<Item = R, Error = E>>;
+
+#[derive(Debug)]
+pub enum ActorError {
+    /// Cannot send message to the actor
+    InvokeError,
+    /// Response was cancelled before being received.
+    WaitError,
+}
+
+impl Error for ActorError {
+    fn description(&self) -> &str {
+        match self {
+            &ActorError::InvokeError => "Cannot send message to actor",
+            &ActorError::WaitError => "Cannot wait for an answer",
+        }
+    }
+}
+
+impl fmt::Display for ActorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{}", self.description())
+    }
+}
+
+impl<T> From<mpsc::SendError<T>> for ActorError {
+    fn from(_from: mpsc::SendError<T>) -> Self {
+        ActorError::InvokeError
+    }
+}
+
+impl From<oneshot::Canceled> for ActorError {
+    fn from(_from: oneshot::Canceled) -> Self {
+        ActorError::WaitError
+    }
 }
