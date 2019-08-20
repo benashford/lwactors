5b2a1cbba363d1083668d491a5d340b2f33d4b4a
Ben Ashford <benashford@users.noreply.github.com>
2019-08-20 16:07:08 +0100

    New implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,136 +1,114 @@
-// use std::{error::Error, fmt};
+#![feature(async_await)]
 
-// use futures::{
-//     future::{self, Executor},
-//     sync::{mpsc, oneshot},
-//     Async, Future, Poll, Stream,
-// };
+use std::{error::Error, fmt};
 
-// /// Construct a new actor, requires an `Executor` and an initial state.  Returns a reference that can be cheaply
-// /// cloned and passed between threads.  A specific implementation is expected to wrap this return value and implement
-// /// the required custom logic.
-// pub fn actor<EX, A, S, R, E>(executor: &EX, initial_state: S) -> ActorSender<A, R, E>
-// where
-//     A: Action<State = S, Result = R, Error = E> + Send + 'static,
-//     S: Send + 'static,
-//     R: Send + 'static,
-//     E: Send + 'static,
-//     EX: Executor<Box<dyn Future<Item = (), Error = ()> + Send>> + 'static,
-// {
-//     let (tx, rx) = mpsc::unbounded();
-//     let actor = Box::new(Actor {
-//         receiver: rx,
-//         state: initial_state,
-//     });
-//     executor
-//         .execute(actor)
-//         .expect("Cannot schedule actor on executor");
-//     ActorSender(tx)
-// }
+use futures::{
+    channel::{mpsc, oneshot},
+    stream::StreamExt,
+    task::SpawnExt,
+};
 
-// type ActorSenderInner<A, R, E> = mpsc::UnboundedSender<(A, oneshot::Sender<Result<R, E>>)>;
+/// Construct a new actor, requires an `Executor` and an initial state.  Returns a reference that can be cheaply
+/// cloned and passed between threads.  A specific implementation is expected to wrap this return value and implement
+/// the required custom logic.
+pub fn actor<A, S, R, E>(mut executor: impl SpawnExt, initial_state: S) -> ActorSender<A, R, E>
+where
+    A: Action<State = S, Result = R, Error = E> + Send + 'static,
+    S: Send + 'static,
+    R: Send + 'static,
+    E: Send + 'static,
+{
+    let (tx, rx) = mpsc::unbounded();
+    executor
+        .spawn(actor_future(rx, initial_state))
+        .expect("Cannot schedule actor on executor");
+    ActorSender(tx)
+}
 
-// #[derive(Debug)]
-// pub struct ActorSender<A, R, E>(ActorSenderInner<A, R, E>);
+type ActorSenderInner<A, R, E> = mpsc::UnboundedSender<(A, oneshot::Sender<Result<R, E>>)>;
 
-// impl<A, R, E> Clone for ActorSender<A, R, E> {
-//     fn clone(&self) -> Self {
-//         ActorSender(self.0.clone())
-//     }
-// }
+#[derive(Debug)]
+pub struct ActorSender<A, R, E>(ActorSenderInner<A, R, E>);
 
-// impl<A, R, E> ActorSender<A, R, E>
-// where
-//     A: Send + 'static,
-//     R: Send + 'static,
-//     E: Send + From<ActorError> + 'static,
-// {
-//     /// Invokes a specific action on the actor.  Returns a future that completes when the actor has
-//     /// performed the action
-//     pub fn invoke(&self, action: A) -> ActFuture<R, E> {
-//         let (tx, rx) = oneshot::channel();
-//         if let Err(e) = self.0.unbounded_send((action, tx)) {
-//             return Box::new(future::err(ActorError::from(e).into()));
-//         }
-//         let recv_f = rx.then(|r| {
-//             future::result(match r {
-//                 Ok(Ok(r)) => Ok(r),
-//                 Ok(Err(e)) => Err(e),
-//                 Err(e) => Err(ActorError::from(e).into()),
-//             })
-//         });
-//         Box::new(recv_f)
-//     }
-// }
+impl<A, R, E> Clone for ActorSender<A, R, E> {
+    fn clone(&self) -> Self {
+        ActorSender(self.0.clone())
+    }
+}
 
-// struct Actor<A, S, R, E> {
-//     receiver: mpsc::UnboundedReceiver<(A, oneshot::Sender<Result<R, E>>)>,
-//     state: S,
-// }
+impl<A, R, E> ActorSender<A, R, E>
+where
+    A: Send + 'static,
+    R: Send + 'static,
+    E: Send + From<ActorError> + 'static,
+{
+    /// Invokes a specific action on the actor.  Returns a future that completes when the actor has
+    /// performed the action
+    pub async fn invoke(&self, action: A) -> ActResult<R, E> {
+        let (tx, rx) = oneshot::channel();
+        if let Err(e) = self.0.unbounded_send((action, tx)) {
+            return Err(ActorError::from(e).into());
+        }
+        match rx.await {
+            Ok(Ok(r)) => Ok(r),
+            Ok(Err(e)) => Err(e),
+            Err(e) => Err(ActorError::from(e).into()),
+        }
+    }
+}
 
-// impl<A, S, R, E> Future for Actor<A, S, R, E>
-// where
-//     A: Action<State = S, Result = R, Error = E>,
-// {
-//     type Item = ();
-//     type Error = ();
+async fn actor_future<A, S, R, E>(
+    mut receiver: mpsc::UnboundedReceiver<(A, oneshot::Sender<Result<R, E>>)>,
+    mut state: S,
+) where
+    A: Action<State = S, Result = R, Error = E>,
+{
+    while let Some((a, tx)) = receiver.next().await {
+        let _ = tx.send(a.act(&mut state));
+    }
+}
 
-//     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-//         loop {
-//             match self.receiver.poll() {
-//                 Ok(Async::Ready(Some((a, tx)))) => {
-//                     // Not checking the result, as nothing may be waiting
-//                     let _ = tx.send(a.act(&mut self.state));
-//                 }
-//                 Ok(Async::Ready(None)) => return Ok(Async::Ready(())),
-//                 Ok(Async::NotReady) => return Ok(Async::NotReady),
-//                 Err(()) => return Err(()),
-//             }
-//         }
-//     }
-// }
+pub trait Action {
+    type State;
+    type Result;
+    type Error;
 
-// pub trait Action {
-//     type State;
-//     type Result;
-//     type Error;
+    fn act(self, s: &mut Self::State) -> Result<Self::Result, Self::Error>;
+}
 
-//     fn act(self, s: &mut Self::State) -> Result<Self::Result, Self::Error>;
-// }
+pub type ActResult<R, E> = Result<R, E>;
 
-// pub type ActFuture<R, E> = Box<dyn Future<Item = R, Error = E> + Send>;
+#[derive(Debug)]
+pub enum ActorError {
+    /// Cannot send message to the actor
+    InvokeError,
+    /// Response was cancelled before being received.
+    WaitError,
+}
 
-// #[derive(Debug)]
-// pub enum ActorError {
-//     /// Cannot send message to the actor
-//     InvokeError,
-//     /// Response was cancelled before being received.
-//     WaitError,
-// }
+impl Error for ActorError {
+    fn description(&self) -> &str {
+        match self {
+            ActorError::InvokeError => "Cannot send message to actor",
+            ActorError::WaitError => "Cannot wait for an answer",
+        }
+    }
+}
 
-// impl Error for ActorError {
-//     fn description(&self) -> &str {
-//         match self {
-//             ActorError::InvokeError => "Cannot send message to actor",
-//             ActorError::WaitError => "Cannot wait for an answer",
-//         }
-//     }
-// }
+impl fmt::Display for ActorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{}", self.description())
+    }
+}
 
-// impl fmt::Display for ActorError {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-//         write!(f, "{}", self.description())
-//     }
-// }
+impl<T> From<mpsc::TrySendError<T>> for ActorError {
+    fn from(_from: mpsc::TrySendError<T>) -> Self {
+        ActorError::InvokeError
+    }
+}
 
-// impl<T> From<mpsc::SendError<T>> for ActorError {
-//     fn from(_from: mpsc::SendError<T>) -> Self {
-//         ActorError::InvokeError
-//     }
-// }
-
-// impl From<oneshot::Canceled> for ActorError {
-//     fn from(_from: oneshot::Canceled) -> Self {
-//         ActorError::WaitError
-//     }
-// }
+impl From<oneshot::Canceled> for ActorError {
+    fn from(_from: oneshot::Canceled) -> Self {
+        ActorError::WaitError
+    }
+}
